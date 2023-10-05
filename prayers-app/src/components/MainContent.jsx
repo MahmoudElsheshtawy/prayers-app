3ab19a4db793efe945a90172ef419557092554d6
mahmoud elsheshtawy <103583850+MahmoudElsheshtawy@users.noreply.github.com>
2023-10-05 23:19:12 +0300

    section-to

--- prayers-app/src/components/MainContent.jsx
+++ prayers-app/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 import { Box, Divider, FormControl, Grid, InputLabel, MenuItem, Select, Stack } from "@mui/material"
 import Prayers from "./prayers"
 import axios from "axios"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const MainContent = () => {
 
@@ -14,24 +14,38 @@ const MainContent = () => {
   Isha:"19:57"
 
   })
-// lojc
-// handle city
- const handleChange=(e)=>{
+  // logic
+  const [city ,setCity]=useState({
+    displayname:"البحيره", apiname:"ElBeheira"
+  })
+
+
+  const clectedcity =[
+    {displayname:"البحيره", apiname:"ElBeheira"},
+    {displayname:"القاهره", apiname:"Cairo"},
+    {displayname:"الاسكندريه", apiname:"Alexandria"}
+    
+  ]
+ const handleCityChange=(e)=>{
   console.log(e.target.value)
+  const cityOpject = clectedcity.find((city)=>{
+    return city.apiname == e.target.value;
+  })
+  setCity(cityOpject)
  }
-axios.get("https://api.aladhan.com/v1/timingsByCity?city=ElBeheira&country=Eg")
-.then(res =>{
-  console.log(res.data.data.timings)
-  setTimings(res.data.data.timings)
-  
-})
+
+ const getTimings = async()=>{
+  const response = await axios.get(`https://api.aladhan.com/v1/timingsByCity?city=${city.apiname}&country=Eg`);
+  console.log(response.data.data.timings.Asr)
+  setTimings(response.data.data.timings)
+ }
+
+useEffect(()=>{
+
+  getTimings()
 
 
-
-
-
-
-
+},[city])
 
 
   return (
@@ -41,7 +55,7 @@ axios.get("https://api.aladhan.com/v1/timingsByCity?city=ElBeheira&country=Eg")
     <Grid container >
     <Grid item xs={6} >
          <h2>أكتوبر 2023|4:20</h2>
-         <h1>الاسكندريه</h1>
+         <h1>{city.displayname} </h1>
     </Grid>
       <Grid item xs={6}>
       <h2>متبقي حتي صلاه الفجر</h2>
@@ -56,20 +70,20 @@ axios.get("https://api.aladhan.com/v1/timingsByCity?city=ElBeheira&country=Eg")
 				justifyContent={"space-around"}
 				style={{ marginTop: "50px" }}>
     <Prayers 
-    name={timings.Fajr}
-    time="03:50"
+    name={'الفجر'}
+    time={timings.Fajr}
     image="https://images.pexels.com/photos/16973076/pexels-photo-16973076/free-photo-of-silhouetted-mosque-at-sunset.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
     <Prayers name="الظهر"
-					time="03:50"
+					time={timings.Dhuhr}
 					image="https://images.pexels.com/photos/337901/pexels-photo-337901.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"/>
     <Prayers name="العصر"
-					time="03:50"
+					time={timings.Asr}
 					image="https://images.pexels.com/photos/14337323/pexels-photo-14337323.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"/>
     <Prayers name="المغرب"
-					time="03:50"
+					time={timings.Maghrib}
 					image="https://images.pexels.com/photos/4348424/pexels-photo-4348424.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
     <Prayers name="العشاء"
-					time="04:30"
+					time={timings.Isha}
 					image="https://images.pexels.com/photos/4055277/pexels-photo-4055277.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"/>
     
    </Stack>
@@ -83,11 +97,12 @@ axios.get("https://api.aladhan.com/v1/timingsByCity?city=ElBeheira&country=Eg")
           // value={age}
           label="Age"
          style={{ color:"#fff"}}
-          onChange={handleChange}
+          onChange={handleCityChange}
         >
-          <MenuItem value={10}>alx</MenuItem>
-          <MenuItem value={20}>ciro</MenuItem>
-          <MenuItem value={30}>bhara</MenuItem>
+          {clectedcity.map((c)=>(
+            <MenuItem key={c.id} value={c.apiname}>{c.displayname}</MenuItem>
+          ))}
+          
         </Select>
       </FormControl>
     </Box>
